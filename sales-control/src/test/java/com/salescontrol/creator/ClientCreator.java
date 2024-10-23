103adcc9c994ade96edd2451b9882be52e08eab8
Matheus Alan <matheusalanpv@outlook.com>
2024-10-23 14:09:15 -0300

    Testes para save, saveAll, listAll

--- sales-control/src/test/java/com/salescontrol/creator/ClientCreator.java
+++ sales-control/src/test/java/com/salescontrol/creator/ClientCreator.java
@@ -29,22 +29,30 @@ public class ClientCreator {
                 .build();
     }
 
-    public static Client createValidClient3() {
-        return Client.builder()
-                .idClient(3)
-                .name("Cliente 3")
-                .nickname("Apelido 3")
-                .cpf("CPF 3")
-                .totalQuantity(0.0)
-                .totalPurchased(BigDecimal.ZERO)
-                .build();
-    }
+//    public static Client createValidClient3() {
+//        return Client.builder()
+//                .idClient(3)
+//                .name("Cliente 3")
+//                .nickname("Apelido 3")
+//                .cpf("CPF 3")
+//                .totalQuantity(0.0)
+//                .totalPurchased(BigDecimal.ZERO)
+//                .build();
+//    }
 
     public static ClientPostDTO createClientPostDTO() {
         return ClientPostDTO.builder()
                 .name("Cliente DTO 1")
                 .nickname("Apelido DTO 1")
                 .cpf("CPF DTO 1")
+                .build();
+    }
+
+    public static ClientPostDTO createClientPostDTO2() {
+        return ClientPostDTO.builder()
+                .name("Cliente DTO 2")
+                .nickname("Apelido DTO 2")
+                .cpf("CPF DTO 2")
                 .build();
     }
 
--- sales-control/src/test/java/com/salescontrol/service/ClientServiceTest.java
+++ sales-control/src/test/java/com/salescontrol/service/ClientServiceTest.java
@@ -9,13 +9,15 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(MockitoExtension.class)
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
 class ClientServiceTest {
 
     @InjectMocks
@@ -24,17 +26,20 @@ class ClientServiceTest {
     @Mock
     private ClientRepository clientRepositoryMock;
 
-//    @BeforeEach
-//    void setUp() {
+    @BeforeEach
+    void setUp() {
 //        BDDMockito.when(clientRepositoryMock.save(ArgumentMatchers.any(Client.class)))
 //                .thenReturn(ClientCreator.createValidClient());
-//    }
+
+        BDDMockito.when(clientRepositoryMock.findAll())
+                .thenReturn(List.of(ClientCreator.createValidClient()));
+    }
 
     @Test
     @DisplayName("When successful must save a client")
     void saveClient_WhenSuccessful() {
         ClientPostDTO clientPostDTOMock = ClientCreator.createClientPostDTO();
-        ClientPostDTO clientPostDTO = clientService.saveClient(ClientCreator.createClientPostDTO());
+        ClientPostDTO clientPostDTO = clientService.saveClient(clientPostDTOMock);
 
         Assertions.assertThat(clientPostDTO.getName()).isNotNull()
                 .isEqualTo(clientPostDTOMock.getName());
@@ -43,11 +48,26 @@ class ClientServiceTest {
     @Test
     @DisplayName("When successful must save all clients")
     void saveAllClients_WhenSuccessful() {
+        List<ClientPostDTO> multipleClients = new ArrayList<>();
         ClientPostDTO clientPostDTOMock = ClientCreator.createClientPostDTO();
-        ClientPostDTO clientPostDTO = clientService.saveClient(ClientCreator.createClientPostDTO());
+        ClientPostDTO clientPostDTOMock2 = ClientCreator.createClientPostDTO();
+        multipleClients.add(clientPostDTOMock);
+        multipleClients.add(clientPostDTOMock2);
 
-        Assertions.assertThat(clientPostDTO.getName()).isNotNull()
-                .isEqualTo(clientPostDTOMock.getName());
+        List<ClientPostDTO> clientPostDTOS = clientService.saveMultipleClients(multipleClients);
+
+        Assertions.assertThat(clientPostDTOS).isNotNull().isNotEmpty();
+        Assertions.assertThat(clientPostDTOS.get(0).getName()).isEqualTo(clientPostDTOMock.getName());
+    }
+
+    @Test
+    @DisplayName("When successful must list all clients")
+    void listAllClients_WhenSuccessful() {
+        Client validClient = ClientCreator.createValidClient();
+        List<Client> clients = clientService.listClients();
+
+        Assertions.assertThat(clients).isNotNull().isNotEmpty();
+        Assertions.assertThat(clients.get(0).getName()).isEqualTo(validClient.getName());
     }
 
 }
