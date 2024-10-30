a1380c6179a03e4c303275f45bbc2357d36f89c7
Matheus Alan <matheusalanpv@outlook.com>
2024-10-29 22:33:33 -0300

    ...

--- sales-control/src/test/java/com/salescontrol/service/ClientServiceTest.java
+++ sales-control/src/test/java/com/salescontrol/service/ClientServiceTest.java
@@ -1,6 +1,7 @@
 package com.salescontrol.service;
 
 import com.salescontrol.creator.ClientCreator;
+import com.salescontrol.dto.client.ClientGetDTO;
 import com.salescontrol.dto.client.ClientPostDTO;
 import com.salescontrol.model.Client;
 import com.salescontrol.repository.ClientRepository;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,6 +18,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 class ClientServiceTest {
@@ -33,16 +36,25 @@ class ClientServiceTest {
 
         BDDMockito.when(clientRepositoryMock.findAll())
                 .thenReturn(List.of(ClientCreator.createValidClient()));
+
+        BDDMockito.when(clientRepositoryMock.findByName(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(ClientCreator.createValidClient()));
+
+        BDDMockito.when(clientRepositoryMock.findByNickname(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(ClientCreator.createValidClient()));
+
+        BDDMockito.when(clientRepositoryMock.findByCpf(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(ClientCreator.createValidClient()));
     }
 
+//    @DirtiesContext
     @Test
     @DisplayName("When successful must save a client")
     void saveClient_WhenSuccessful() {
         ClientPostDTO clientPostDTOMock = ClientCreator.createClientPostDTO();
         ClientPostDTO clientPostDTO = clientService.saveClient(clientPostDTOMock);
 
-        Assertions.assertThat(clientPostDTO.getName()).isNotNull()
-                .isEqualTo(clientPostDTOMock.getName());
+        Assertions.assertThat(clientPostDTO.getName()).isNotNull().isEqualTo(clientPostDTOMock.getName());
     }
 
     @Test
@@ -65,9 +77,41 @@ class ClientServiceTest {
     void listAllClients_WhenSuccessful() {
         Client validClient = ClientCreator.createValidClient();
         List<Client> clients = clientService.listClients();
+        Client client = clientService.listClients().get(0);
 
         Assertions.assertThat(clients).isNotNull().isNotEmpty();
         Assertions.assertThat(clients.get(0).getName()).isEqualTo(validClient.getName());
+        Assertions.assertThat(clients).contains(client);
+    }
+
+    @Test
+    @DisplayName("When successful must find client by name")
+    void findClientByName_WhenSuccessful() {
+        Client validClient = ClientCreator.createValidClient();
+        ClientGetDTO responseClientByName = clientService.findByName(validClient.getName());
+
+        Assertions.assertThat(responseClientByName).isNotNull();
+        Assertions.assertThat(responseClientByName.getName()).isEqualTo(validClient.getName());
+    }
+
+    @Test
+    @DisplayName("When successful must find client by nickname")
+    void findClientByNickname_WhenSuccessful() {
+        Client validClient = ClientCreator.createValidClient();
+        ClientGetDTO responseClientByNickname = clientService.findByNickname(validClient.getNickname());
+
+        Assertions.assertThat(responseClientByNickname).isNotNull();
+        Assertions.assertThat(responseClientByNickname.getNickname()).isEqualTo(validClient.getNickname());
+    }
+
+    @Test
+    @DisplayName("When successful must find client by CPF")
+    void findClientByCPF_WhenSuccessful() {
+        Client validClient = ClientCreator.createValidClient();
+        ClientGetDTO responseClientByCPF = clientService.findByCpf(validClient.getCpf());
+
+        Assertions.assertThat(responseClientByCPF).isNotNull();
+        Assertions.assertThat(responseClientByCPF.getCpf()).isEqualTo(validClient.getCpf());
     }
 
 }
