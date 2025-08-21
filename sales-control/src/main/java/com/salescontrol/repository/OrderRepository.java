48e56d36154212e9bdceb3a1e5a8ebec566076c3
Matheus Alan <matheusalanpv@outlook.com>
2025-08-21 20:57:42 -0300

    organizando arquivos json

--- sales-control/src/main/java/com/salescontrol/repository/OrderRepository.java
+++ sales-control/src/main/java/com/salescontrol/repository/OrderRepository.java
@@ -6,17 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
-
-//    Para as quantidades mais solicitadas
-    @Query("SELECT o.quantity, COUNT(o) FROM Order o GROUP BY o.quantity ORDER BY COUNT(o) DESC")
-    List<Object[]> findMostRequestedQuantities();
-
-//    @Query("SELECT COALESCE(SUM(o.idOrder), 0) FROM ORDER o")
-//    Integer sumTotalOrders();
 
     @Query("SELECT COUNT(o) FROM Order o")
     Integer sumTotalOrders();
